101d9b2b4543991121e1ba732739572e4ede80e6
draiguv <dcrauv13@gmail.com>
2022-12-11 17:45:49 +0800

    Add UI interactions

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ let computerStanding = 0;
 function getSelectedValue(selected) {
     return selected == 1 ? 'rock'
         : selected == 2 ? 'paper'
-        : 'scissors'
+        : 'scissors';
 }
 
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3) + 1
+    return Math.floor(Math.random() * 3) + 1;
 }
 
 function validatePlayerChoice(choice) {
-    if (choice == 'rock' || choice == 'paper' || choice == 'scissors') return true
+    if (choice == 'rock' || choice == 'paper' || choice == 'scissors') return true;
     
-    alert('Invalid choice! Choose again.')
+    alert('Invalid choice! Choose again.');
     playerSelection = prompt('Rock? Paper? Scissors? Choose your PokéHand! Good luck!');
 
     if (playerSelection == null) {
-        if (confirm('You cancelled, do you surrender?')) return
+        if (confirm('You cancelled, do you surrender?')) return;
         playerSelection = prompt('Rock? Paper? Scissors? Choose your PokéHand! Good luck!');
     }
     
@@ -27,33 +27,80 @@ function validatePlayerChoice(choice) {
 }
 
 function playRound(playerSelection, computerSelection) {
+    let log;
+    
+    playerSelection = playerSelection.target.innerText.toLowerCase();
+    computerSelection = getSelectedValue(getComputerChoice());
+
     // DRAW SECTION
     if (playerSelection == 'rock' && computerSelection == 'rock') {
-        console.log('Draw!');
+        log = 'Draw!';
     } else if (playerSelection == 'paper' && computerSelection == 'paper') {
-        console.log('Draw!');
+        log = 'Draw!';
     } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
-        console.log('Draw!');
+        log = 'Draw!';
     }
     
     else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        console.log('You Lose! Rock is helpless to Paper!');
+        log = 'You Lose! Rock is helpless to Paper!';
         computerStanding += 1;
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        console.log('You Win! Rock breaks Scissors!');
+        log = 'You Win! Rock breaks Scissors!';
         playerStanding += 1;
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        console.log('You Win! Paper have magic to defeat Rock!');
+        log = 'You Win! Paper have magic to defeat Rock!';
         playerStanding += 1;
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        console.log('You Lose! Scissors cuts Paper!');
+        log = 'You Lose! Scissors cuts Paper!';
         computerStanding += 1;
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        console.log('You Lose! Scissors can\'t cut Rock idiot!');
+        log = 'You Lose! Scissors can\'t cut Rock idiot!';
         computerStanding += 1;
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        console.log('You Win! Scissors is super effective against Paper!');
+        log = 'You Win! Scissors is super effective against Paper!';
         playerStanding += 1;
+    }
+
+    const div = document.querySelector('.result');
+    const player = document.createElement('p');
+    const computer = document.createElement('p');
+    const logs = document.createElement('p');
+
+    const playerClass = document.querySelector('.player');
+    const computerClass = document.querySelector('.computer');
+    const logsClass = document.querySelector('.logs');
+
+    if (playerClass != null) div.removeChild(playerClass);
+    if (computerClass != null) div.removeChild(computerClass);
+    if (logsClass != null) div.removeChild(logsClass);
+
+    logs.textContent = log;
+    div.appendChild(logs);
+
+    player.textContent = `You: ${playerStanding}`;
+    computer.textContent = `Computer: ${computerStanding}`;
+
+    div.appendChild(player);
+    div.appendChild(computer);
+
+    logs.classList.add('logs');
+    player.classList.add('player');
+    computer.classList.add('computer');
+
+    if (playerStanding == 5 || computerStanding == 5) {
+        const finalResult = document.createElement('h2');
+        const finalResultClass = document.querySelector('.finalResult');
+
+        if (finalResultClass != null) div.removeChild(finalResultClass);
+
+        finalResult.textContent = playerStanding == 5 ? 'Congratulation! You beaten the computer' : 'You Lose, loser! LOL XD';
+        finalResult.classList.add('finalResult');
+    
+        div.appendChild(finalResult);
+
+        setTimeout(() => {
+            resetGame();
+        }, 2000);
     }
 }
 
@@ -65,7 +112,7 @@ function game() {
         playerSelection = prompt('Rock? Paper? Scissors? Choose your PokéHand! Good luck!');
 
         if (playerSelection == null) {
-            if (confirm('You cancelled, do you surrender?')) return
+            if (confirm('You cancelled, do you surrender?')) return;
             playerSelection = prompt('Rock? Paper? Scissors? Choose your PokéHand! Good luck!');
         }
         
@@ -77,5 +124,27 @@ function game() {
     }
 }
 
-game();
-if (playerSelection != null) console.log(playerStanding == 5 ? 'Congratulation! You beaten the computer' : 'You Lose, loser! LOL XD');
+// game();
+// if (playerSelection != null) console.log(playerStanding == 5 ? 'Congratulation! You beaten the computer' : 'You Lose, loser! LOL XD', ` - Standing: You = ${playerStanding} | Computer = ${computerStanding}`);
+
+function resetGame() {
+    const div = document.querySelector('.result');
+    const player = document.querySelector('.player');
+    const computer = document.querySelector('.computer');
+    const logs = document.querySelector('.logs');
+    const finalResult = document.querySelector('.finalResult');
+
+    div.removeChild(player);
+    div.removeChild(computer);
+    div.removeChild(logs);
+    div.removeChild(finalResult);
+
+    playerStanding = 0;
+    computerStanding = 0;
+}
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', playRound);
+});
